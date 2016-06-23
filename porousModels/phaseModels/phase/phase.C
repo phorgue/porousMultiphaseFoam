786c8174d6210c048fcfe05ade2c3360387d7bcb
Pierre Horgue <phorgue@gmail.com>
2016-06-23 13:28:37 +0200

    minor changes in phaseModel => preparation for compressible solver

---
+++ porousModels/phaseModels/phase/phase.C
@@ -0,0 +1,60 @@
+/*---------------------------------------------------------------------------*\
+  =========                 |
+  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
+   \\    /   O peration     |
+    \\  /    A nd           | Copyright (C) 1991-2010 OpenCFD Ltd.
+     \\/     M anipulation  |
+-------------------------------------------------------------------------------
+License
+    This file is part of OpenFOAM.
+
+    OpenFOAM is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenFOAM.  If not, see <http://www.gnu.org/licenses/>.
+
+\*---------------------------------------------------------------------------*/
+
+#include "phase.H"
+
+// * * * * * * * * * * * * * * * * Constructors  * * * * * * * * * * * * * * //
+
+Foam::phase::phase
+(
+    const fvMesh& mesh,
+    const dictionary& transportProperties,
+    const word& phaseName
+)
+:  
+    mesh_(mesh),
+    dict_(transportProperties.subDict("phase."+phaseName)),
+    name_(phaseName)
+{
+}
+
+Foam::autoPtr<Foam::phase> Foam::phase::New
+(
+    const fvMesh& mesh,
+    const dictionary& transportProperties,
+    const word& phaseName
+)
+{
+    return autoPtr<phase>
+    (
+        new phase(mesh, transportProperties, "phase."+phaseName)
+    );
+}
+
+// * * * * * * * * * * * * * * * * Destructor  * * * * * * * * * * * * * * * //
+
+Foam::phase::~phase()
+{}
+
