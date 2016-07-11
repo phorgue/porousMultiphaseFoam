88a280f7966774f589855f321ed95467d4db7902
Pierre Horgue <phorgue@gmail.com>
2016-07-11 15:45:56 +0200

    python script for running tutorials

---
+++ tutorials/tutorialsList.py
@@ -0,0 +1,13 @@
+# tutorials list for python validation script
+
+tutorials = [{'solver' : "impesFoam", 'case' : "Buckley-Leverett/BrooksAndCorey"}, \
+             {'solver' : "impesFoam", 'case' : "Buckley-Leverett/VanGenuchten"}, \
+             {'solver' : "impesFoam", 'case' : "capillarityValidation/BrooksAndCorey"}, \
+             {'solver' : "impesFoam", 'case' : "capillarityValidation/VanGenuchten"}, \
+             {'solver' : "impesFoam", 'case' : "injectionExtraction/injection"}, \
+             {'solver' : "impesFoam", 'case' : "injectionExtraction/extraction"}, \
+             {'solver' : "anisoImpesFoam", 'case' : "injectionAniso_case1"}, \
+             {'solver' : "anisoImpesFoam", 'case' : "injectionAniso_case2"}, \
+             {'solver' : "groundwaterFoam", 'case' : "1Dinfiltration"}, \
+             {'solver' : "groundwaterFoam", 'case' : "1Dinfiltration_Ufixed"}, \
+             {'solver' : "groundwaterFoam", 'case' : "realCase"}]
