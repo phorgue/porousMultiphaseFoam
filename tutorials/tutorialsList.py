67a8afe3785b939005d9e5e98aef83b4a17099e8
Pierre Horgue <phorgue@gmail.com>
2017-10-18 12:20:31 +0200

    new extend branch updated to v1.5 version

--- tutorials/tutorialsList.py
+++ tutorials/tutorialsList.py
@@ -2,14 +2,11 @@
 
 tutorials = [{'solver' : "impesFoam", 'case' : "Buckley-Leverett/BrooksAndCorey"}, \
              {'solver' : "impesFoam", 'case' : "Buckley-Leverett/VanGenuchten"}, \
-             {'solver' : "impesFoam", 'case' : "capillarityValidation/BrooksAndCorey"}, \
-             {'solver' : "impesFoam", 'case' : "capillarityValidation/VanGenuchten"}, \
              {'solver' : "impesFoam", 'case' : "injectionExtraction/injection"}, \
              {'solver' : "impesFoam", 'case' : "injectionExtraction/extraction"}, \
              {'solver' : "anisoImpesFoam", 'case' : "injectionAniso_case1"}, \
              {'solver' : "anisoImpesFoam", 'case' : "injectionAniso_case2"}, \
              {'solver' : "groundwaterFoam", 'case' : "1Dinfiltration"}, \
              {'solver' : "groundwaterFoam", 'case' : "1Dinfiltration_Ufixed"}, \
-             {'solver' : "groundwaterFoam", 'case' : "realCase"}, \
              {'solver' : "darcyFoam", 'case' : "SPE10"}, \
              {'solver' : "porousScalarTransportFoam", 'case' : "alphaDispersion"}]
