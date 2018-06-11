8b07924303bb9990d8bb4e067b27ff8f57cb574c
Pierre Horgue <phorgue@gmail.com>
2018-06-11 16:04:03 +0200

    dtheta_avg added in output

--- solvers/groundwaterFoam/groundwaterFoam.C
+++ solvers/groundwaterFoam/groundwaterFoam.C
@@ -85,6 +85,7 @@ int main(int argc, char *argv[])
         Info << "Saturation theta " << " Min(theta) = " << gMin(theta.internalField()) << " Max(theta) = " << gMax(theta.internalField()) <<  endl;
         Info << "Head pressure h  " << " Min(h) = " << gMin(h.internalField()) << " Max(h) = " << gMax(h.internalField()) <<  endl;
         dthetadTmax = gMax(mag(theta-theta.oldTime())())/runTime.deltaTValue();
+        dtheta_avg = (theta-theta.oldTime())().weightedAverage(mesh.V()).value();
 
         runTime.write();
 
