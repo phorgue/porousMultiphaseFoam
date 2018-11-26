c2e554230f275d00a1a59a2a4aa4319c6f6260b4
Pierre Horgue <phorgue@gmail.com>
2018-11-26 11:39:04 +0100

    add fixed points in stationary 2D solver (with bilan)

--- solvers/stationaryGroundwater2DFoam/stationaryGroundwater2DFoam.C
+++ solvers/stationaryGroundwater2DFoam/stationaryGroundwater2DFoam.C
@@ -47,6 +47,7 @@ int main(int argc, char *argv[])
     #include "createMesh.H"
     simpleControl simple(mesh);
     #include "createFields.H"
+    #include "readFixedPoints.H"
 
     // * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
 
