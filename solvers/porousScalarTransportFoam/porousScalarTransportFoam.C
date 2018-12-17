90589c69ed4b52fb5d37ad9c4bec71f6377a010b
Pierre Horgue <phorgue@gmail.com>
2018-12-17 15:47:43 +0100

    C bilan added to porous scalar transport

--- solvers/porousScalarTransportFoam/porousScalarTransportFoam.C
+++ solvers/porousScalarTransportFoam/porousScalarTransportFoam.C
@@ -59,6 +59,7 @@ int main(int argc, char *argv[])
         Info << "Time = " << runTime.timeName() << nl << endl;
 
         #include "CEqn.H"
+        #include "CmassBalance.H"
 
         runTime.write();
     }
