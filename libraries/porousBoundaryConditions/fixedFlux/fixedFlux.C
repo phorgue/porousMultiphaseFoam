977b52779c15ca6fb9733c6656a347dc4142edd5
Pierre Horgue <phorgue@gmail.com>
2019-01-23 17:11:23 +0100

    add event to fixedFlux

--- libraries/porousBoundaryConditions/fixedFlux/fixedFlux.C
+++ libraries/porousBoundaryConditions/fixedFlux/fixedFlux.C
@@ -35,10 +35,12 @@ fixedFlux
     const fvPatch& h,
     const DimensionedField<scalar, volMesh>& iF
 )
-:
+    :
     fixedValueFvPatchScalarField(h, iF),
     fixedFluxValue_(0.),
-    phiName_("phi")
+    phiName_("phi"),
+    iterEvent_(-1),
+    valueEvent_(0.0)
 {}
 
 
@@ -49,11 +51,20 @@ fixedFlux
     const DimensionedField<scalar, volMesh>& iF,
     const dictionary& dict
 )
-:
+    :
     fixedValueFvPatchScalarField(h, iF, dict, false),
     fixedFluxValue_(dict.lookupOrDefault<scalar>("fixedFluxValue",0.)),
-    phiName_(dict.lookupOrDefault<word>("phiName","phi"))
-{}
+    phiName_(dict.lookupOrDefault<word>("phiName","phi")),
+    iterEvent_(-1),
+    valueEvent_(0.0)
+{
+    if (dict.found("event"))
+    {
+        List<Tuple2<scalar, scalar> > eventRead= dict.lookup("event");
+        eventData_ = eventRead;
+        iterEvent_ = 0;
+    }
+}
 
 
 Foam::fixedFlux::
@@ -67,7 +78,9 @@ fixedFlux
 :
     fixedValueFvPatchScalarField(ptf, h, iF, mapper),
     fixedFluxValue_(ptf.fixedFluxValue_),
-    phiName_(ptf.phiName_)
+    phiName_(ptf.phiName_),
+    iterEvent_(-1),
+    valueEvent_(0.0)
 {}
 
 
@@ -79,7 +92,9 @@ fixedFlux
 :
     fixedValueFvPatchScalarField(ptf),
     fixedFluxValue_(ptf.fixedFluxValue_),
-    phiName_(ptf.phiName_)
+    phiName_(ptf.phiName_),
+    iterEvent_(-1),
+    valueEvent_(0.0)
 {}
 
 
@@ -92,7 +107,9 @@ fixedFlux
 :
     fixedValueFvPatchScalarField(ptf, iF),
     fixedFluxValue_(ptf.fixedFluxValue_),
-    phiName_(ptf.phiName_)
+    phiName_(ptf.phiName_),
+    iterEvent_(-1),
+    valueEvent_(0.0)
 {}
 
 
@@ -106,8 +123,20 @@ void Foam::fixedFlux::updateCoeffs()
     }
     const fvsPatchField<scalar>& phip_=
         patch().lookupPatchField<surfaceScalarField, scalar>(phiName_);
+
+    //- Updating event value
+    if ((iterEvent_ >= 0 ) && (iterEvent_<eventData_.size()))
+    {
+        if (eventData_[iterEvent_+1].first() < this->db().time().value())
+        {
+            iterEvent_++;
+            valueEvent_ = eventData_[iterEvent_].second();
+        }
+    }
+
+    //- Computing fixed value
     scalarField results(patch().patch().faceCentres().size());    
-    results = -fixedFluxValue_/sum(phip_);
+    results = (-fixedFluxValue_-valueEvent_)/sum(phip_);
     operator== (results);
     fixedValueFvPatchScalarField::updateCoeffs();
 }
