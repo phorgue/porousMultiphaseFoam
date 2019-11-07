553ba7f3ca3154e8c17fdb0f5beb78d96525d397
Gabriel Gerlero <gerlero@users.noreply.github.com>
2019-11-07 14:18:50 +0100

    refactor: make eventFlux and eventInfiltration BCs provide solvers with pointers to patchEventFiles
    Replaces the previous method that used an IODictionary for coordination.

--- solvers/groundwaterTransport2DFoam/groundwaterTransport2DFoam.C
+++ solvers/groundwaterTransport2DFoam/groundwaterTransport2DFoam.C
@@ -40,6 +40,8 @@ Developer
 #include "sourceEventFile.H"
 #include "outputEventFile.H"
 #include "patchEventFile.H"
+#include "eventInfiltration.H"
+#include "eventFlux.H"
 
 // * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
 using namespace Foam;
@@ -63,7 +65,7 @@ int main(int argc, char *argv[])
         if (outputEventIsPresent) outputEvent.update(runTime.timeOutputValue());
         if (infiltrationEventIsPresent) infiltrationEvent.update(runTime.timeOutputValue());
         if (sourceEventIsPresent) sourceEvent.update(runTime.timeOutputValue());
-        if (patchEventIsPresent) forAll(patchEventList,patchEventi) patchEventList[patchEventi]->update(runTime.timeOutputValue());
+        forAll(patchEventList,patchEventi) patchEventList[patchEventi]->update(runTime.timeOutputValue());
         #include "setDeltaT.H"
 
         runTime++;
--- solvers/impesFoam/impesFoam.C
+++ solvers/impesFoam/impesFoam.C
@@ -43,6 +43,7 @@ Developers
 #include "sourceEventFile.H"
 #include "outputEventFile.H"
 #include "patchEventFile.H"
+#include "eventInfiltration.H"
 
 // * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
 using namespace Foam;
@@ -67,7 +68,7 @@ int main(int argc, char *argv[])
     {
         if (outputEventIsPresent) outputEvent.update(runTime.timeOutputValue());
         if (sourceEventIsPresent) sourceEvent.update(runTime.timeOutputValue());
-        if (patchEventIsPresent) forAll(patchEventList,patchEventi) patchEventList[patchEventi]->update(runTime.timeOutputValue());
+        forAll(patchEventList,patchEventi) patchEventList[patchEventi]->update(runTime.timeOutputValue());
         #include "setDeltaT.H"
 
         runTime++;
