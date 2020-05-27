6dc4411fede449d459a9ce53f0647c571b58126d
Pierre Horgue <phorgue@gmail.com>
2020-05-27 10:08:38 +0200

    add outputEvent interpolation to multiple solvers

--- solvers/anisoImpesFoam/anisoImpesFoam.C
+++ solvers/anisoImpesFoam/anisoImpesFoam.C
@@ -65,7 +65,6 @@ int main(int argc, char *argv[])
 
     while (runTime.run())
     {
-        if (outputEventIsPresent) outputEvent.updateIndex(runTime.timeOutputValue());
         if (sourceEventIsPresent) sourceEvent.updateIndex(runTime.timeOutputValue());
         forAll(patchEventList,patchEventi) patchEventList[patchEventi]->updateIndex(runTime.timeOutputValue());
         #include "setDeltaT.H"
