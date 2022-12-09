fe3f837b869084636f5cba97759220c3715a6887
Pierre Horgue <phorgue@imft.fr>
2022-12-09 09:35:15 +0100

    fix residual display in groundwater-transport solver

--- solvers/groundwaterTransportFoam/groundwaterTransportFoam.C
+++ solvers/groundwaterTransportFoam/groundwaterTransportFoam.C
@@ -88,7 +88,6 @@ noConvergence :
         #include "computeSourceTerm.H"
         scalar deltahIter = 1;
         scalar hEqnResidual = 1.00001;
-        scalar hEqnResidualSigned = 0;
 
         //- 1) Richard's equation (Picard loop)
         iterPicard = 0;
@@ -97,7 +96,7 @@ noConvergence :
             iterPicard++;
             #include "hEqnPicard.H"
             #include "updateProperties.H"
-            Info << "Picard iteration " << iterPicard << ": max(deltah) = " << deltahIter << ", residual = " << hEqnResidualSigned << endl;
+            Info << "Picard iteration " << iterPicard << ": max(deltah) = " << deltahIter << ", residual = " << hEqnResidual << endl;
         }
         if ( hEqnResidual > tolerancePicard )
         {
@@ -115,7 +114,7 @@ noConvergence :
             iterNewton++;
             #include "hEqnNewton.H"
             #include "checkResidual.H"
-            Info << "Newton iteration " << iterNewton << ": max(deltah) = " << deltahIter << ", residual = " << hEqnResidualSigned << endl;
+            Info << "Newton iteration " << iterNewton << ": max(deltah) = " << deltahIter << ", residual = " << hEqnResidual << endl;
         }
         if ( hEqnResidual > toleranceNewton )
         {
