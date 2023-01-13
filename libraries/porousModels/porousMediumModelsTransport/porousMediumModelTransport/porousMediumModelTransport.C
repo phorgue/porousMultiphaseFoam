cd4198d82f04f0fffab5dad9d2052d3779097ddf
Pierre Horgue <phorgue@imft.fr>
2023-01-13 16:50:04 +0100

    add porousMediumModelTransport abstract class

---
+++ libraries/porousModels/porousMediumModelsTransport/porousMediumModelTransport/porousMediumModelTransport.C
@@ -0,0 +1,57 @@
+/*---------------------------------------------------------------------------*\
+  		  _______  ____    ____  ________  
+ 		 |_   __ \|_   \  /   _||_   __  | 
+   		   | |__) | |   \/   |    | |_ \_| 
+   		   |  ___/  | |\  /| |    |  _|    
+    		  _| |_    _| |_\/_| |_  _| |_     
+   		 |_____|  |_____||_____||_____|    
+   	     Copyright (C) Toulouse INP, Pierre Horgue
+
+License
+    This file is part of porousMultiphaseFoam, an extension of OpenFOAM
+    developed by Pierre Horgue (phorgue@imft.fr) and dedicated to multiphase 
+    flows through porous media.
+
+    OpenFOAM is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenFOAM.  If not, see <http://www.gnu.org/licenses/>.
+
+\*---------------------------------------------------------------------------*/
+
+#include "porousMediumModelTransport.H"
+
+// * * * * * * * * * * * * * * Static Data Members * * * * * * * * * * * * * //
+
+namespace Foam
+{
+defineTypeNameAndDebug(porousMediumModelTransport, 0);
+defineRunTimeSelectionTable(porousMediumModelTransport, dictionary);
+}
+
+// * * * * * * * * * * * * * * * * Constructors  * * * * * * * * * * * * * * //
+
+Foam::porousMediumModelTransport::porousMediumModelTransport
+(
+    const word Sname,
+    const fvMesh& mesh,
+    const dictionary& transportProperties,
+    const porousMediumModel& pmModel,
+    const word mediumName
+)
+    :
+    Sname_(Sname),
+    transportProperties_(transportProperties),
+    pmModel_(pmModel)
+{
+}
+
+// ************************************************************************* //
---
+++ libraries/porousModels/porousMediumModelsTransport/porousMediumModelTransport/porousMediumModelTransportNew.C
@@ -0,0 +1,66 @@
+/*---------------------------------------------------------------------------*\
+  		  _______  ____    ____  ________  
+ 		 |_   __ \|_   \  /   _||_   __  | 
+   		   | |__) | |   \/   |    | |_ \_| 
+   		   |  ___/  | |\  /| |    |  _|    
+    		  _| |_    _| |_\/_| |_  _| |_     
+   		 |_____|  |_____||_____||_____|    
+   	     Copyright (C) Toulouse INP, Pierre Horgue
+
+License
+    This file is part of porousMultiphaseFoam, an extension of OpenFOAM
+    developed by Pierre Horgue (phorgue@imft.fr) and dedicated to multiphase 
+    flows through porous media.
+
+    OpenFOAM is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenFOAM.  If not, see <http://www.gnu.org/licenses/>.
+
+\*---------------------------------------------------------------------------*/
+
+#include "porousMediumModelTransport.H"
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+Foam::autoPtr<Foam::porousMediumModelTransport> Foam::porousMediumModelTransport::New
+(
+    const word Sname,
+    const fvMesh& mesh,
+    const dictionary& transportProperties,
+    const porousMediumModel& pmModel,
+    const word mediumName
+)
+{
+    const word modelType(transportProperties.lookupOrDefault<word>("porousMediumModel", "simplePorosity"));
+
+    Info<< "Selecting porousMedium model => " << modelType << "\n" << endl;
+
+    auto cstrIter = dictionaryConstructorTablePtr_->find(modelType);
+
+    if (cstrIter == dictionaryConstructorTablePtr_->end())
+    {
+        FatalErrorIn
+            (
+                "porousMediumModelTransport::New(...)"
+            )   << "Unknown porousMediumModelTransport type "
+                << modelType << nl << nl
+                << "Valid porousMediumModelTransports are : " << endl
+                << dictionaryConstructorTablePtr_->sortedToc()
+                << exit(FatalError);
+    }
+
+    return autoPtr<porousMediumModelTransport>
+        (cstrIter()(Sname, mesh, transportProperties, pmModel, mediumName));
+}
+
+
+// ************************************************************************* //
