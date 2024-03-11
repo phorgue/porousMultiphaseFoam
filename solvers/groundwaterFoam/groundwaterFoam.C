3fcc0031a9ec4cf051969ff1cea2cc1f30233531
Pierre Horgue <phorgue@imft.fr>
2024-03-11 16:18:25 +0100

    DEV: fix waterMassBalance for Richards' solvers

--- solvers/groundwaterFoam/groundwaterFoam.C
+++ solvers/groundwaterFoam/groundwaterFoam.C
@@ -90,7 +90,7 @@ int main(int argc, char *argv[])
     //- output event
     autoPtr<outputEventFile> outputEvent = outputEventFile::New(runTime);
     outputEvent->addField(h, phi, "m3", false);
-    outputEvent->addField(theta, phi, "m3");
+    outputEvent->addField(theta, phi, "m3", true, true);
 
 
 
@@ -189,10 +189,19 @@ noConvergence :
         scalarField dtheta_tmp = mag(theta.internalField()-theta.oldTime().internalField());
         scalar dtheta = gMax(dtheta_tmp);
 
+        //- water mass balance terminal display
         Info << "Saturation theta: " << " Min(theta) = " << gMin(theta.internalField()) << " Max(theta) = " << gMax(theta.internalField()) << " dthetamax = " << dtheta << endl;
         Info << "Head pressure h: " << " Min(h) = " << gMin(h.internalField()) << " Max(h) = " << gMax(h.internalField()) << endl;
+        Info << "Water mass balance (m3/s) : sourceTerm = " << fvc::domainIntegrate(sourceTerm).value() << " ; ";
+        forAll(phi.boundaryField(),patchi)
+        {
+            if (mesh.boundaryMesh()[patchi].type() == "patch")
+            {
+                Info << phi.boundaryField()[patchi].patch().name() << " = " <<  gSum(phi.boundaryField()[patchi]) << " ; ";
+            }
+        }
+        Info << endl;
 
-        #include "waterMassBalance.H"
         if (steady)
         {
             runTime.write();
