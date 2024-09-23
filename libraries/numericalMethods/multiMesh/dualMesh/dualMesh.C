bc2ad9119a20f6d5548e7083824071603c722b1f
Pierre Horgue <phorgue@imft.fr>
2024-09-23 10:42:20 +0200

    DEV: create template mapFieldCoarseToFine (dualMesh)

--- libraries/numericalMethods/multiMesh/dualMesh/dualMesh.C
+++ libraries/numericalMethods/multiMesh/dualMesh/dualMesh.C
@@ -56,8 +56,8 @@ Foam::dualMesh::dualMesh
     :
     multiMesh(mesh),
     fineMeshPtr_(nullptr),
-    scalarFields_(0),
-    vectorFields_(0)
+    scalarFields_(),
+    vectorFields_()
 {
     //- Read the dual mesh
     fineMeshPtr_ = dynamicFvMesh::New
@@ -84,6 +84,74 @@ Foam::dualMesh::dualMesh
                 }
             }
             refCast<dynamicRefineFvMesh>(fineMesh).protectedCell() = bitSet(boundary_protected_cells);
+        }
+    }
+}
+
+// * * * * * * * * * * * * * * * Private Members  * * * * * * * * * * * * * * //
+
+template<class Type, template<class> class PatchField>
+void Foam::dualMesh::mapFieldCoarseToFine(
+    Foam::GeometricField<Type, PatchField, Foam::volMesh>& field1,
+    Foam::GeometricField<Type, PatchField, Foam::volMesh>& field2
+)
+{
+    dynamicFvMesh& fineMesh = fineMeshPtr_.ref();
+    const refinementHistory& ref_hist = refCast<dynamicRefineFvMesh>(fineMesh).meshCutter().history();
+    const labelIOList& cell_level =  refCast<dynamicRefineFvMesh>(fineMesh).meshCutter().cellLevel();
+    label list_size = ref_hist.splitCells().size();
+
+    List<Type> savedValues(list_size, Foam::Zero);
+    List<Type> savedValuesOld(list_size, Foam::Zero);
+
+    label cell0 = 0;
+    for(label celli=0;celli<coarseMesh_.nCells();celli++)
+    {
+        field2[celli] = field1[celli];
+        field2.oldTime()[celli] = field1.oldTime()[celli];
+
+        // propagation of value
+        if (cell_level[celli] > 0)
+        {
+            for(label iter=0;iter<cell_level[celli];iter++)
+            {
+                cell0 = ref_hist.splitCells()[cell0].parent_;
+            }
+            cell0 /= 9;
+            savedValues[cell0] = field1[celli];
+            savedValuesOld[cell0] = field1.oldTime()[celli];
+        }
+    }
+
+    if (coarseMesh_.nCells() != fineMesh.nCells())
+    {
+        // labels of all new cells created by the remeshing
+        for(label celli=coarseMesh_.nCells();celli<fineMesh.nCells();celli++)
+        {
+            for(label iter=0;iter<cell_level[celli];iter++)
+            {
+                cell0 = ref_hist.splitCells()[cell0].parent_;
+            }
+            cell0 /= 9;
+            field2[celli] = savedValues[cell0];
+            field2.oldTime()[celli] = savedValuesOld[cell0];
+        }
+    }
+
+    forAll(fineMesh.boundary(), patchi)
+    {
+        if (isA<processorPolyPatch>(fineMesh.boundaryMesh()[patchi]))
+        {
+            field2.boundaryFieldRef()[patchi].initEvaluate(Pstream::commsTypes::nonBlocking);
+            field2.oldTime().boundaryFieldRef()[patchi].initEvaluate(Pstream::commsTypes::nonBlocking);
+        }
+        else
+        {
+            forAll(field2.boundaryFieldRef()[patchi], facei)
+            {
+                field2.boundaryFieldRef()[patchi][facei] = field1.boundaryField()[patchi][facei];
+                field2.oldTime().boundaryFieldRef()[patchi][facei] = field1.oldTime().boundaryField()[patchi][facei];
+            }
         }
     }
 }
@@ -116,72 +184,15 @@ void Foam::dualMesh::addDualFields
 
 void Foam::dualMesh::update()
 {
-    dynamicFvMesh& fineMesh = fineMeshPtr_.ref();
-    const refinementHistory& ref_hist = refCast<dynamicRefineFvMesh>(fineMesh).meshCutter().history();
-    const labelIOList& cell_level =  refCast<dynamicRefineFvMesh>(fineMesh).meshCutter().cellLevel();
-    label list_size = ref_hist.splitCells().size();
-
-    label cell0 = 0;
+    fineMeshPtr_.ref().update();
     forAll(scalarFields_, fieldsi)
     {
-        volScalarField& field1 = *scalarFields_[fieldsi].first();
-        volScalarField& field2 = *scalarFields_[fieldsi].second();
-        scalarList savedValues(list_size, 0);
-        scalarList savedValuesOld(list_size, 0);
-
-
-        for(label celli=0;celli<coarseMesh_.nCells();celli++)
-        {
-            field2[celli] = field1[celli];
-            field2.oldTime()[celli] = field1.oldTime()[celli];
-
-            // propagation of value
-            if (cell_level[celli] > 0)
-            {
-                for(label iter=0;iter<cell_level[celli];iter++)
-                {
-                    cell0 = ref_hist.splitCells()[cell0].parent_;
-                }
-                cell0 /= 9;
-                savedValues[cell0] = field1[celli];
-                savedValuesOld[cell0] = field1.oldTime()[celli];
-            }
-        }
-
-        if (coarseMesh_.nCells() != fineMesh.nCells())
-        {
-            // labels of all new cells created by the remeshing
-            for(label celli=coarseMesh_.nCells();celli<fineMesh.nCells();celli++)
-            {
-                for(label iter=0;iter<cell_level[celli];iter++)
-                {
-                    cell0 = ref_hist.splitCells()[cell0].parent_;
-                }
-                cell0 /= 9;
-                field2[celli] = savedValues[cell0];
-                field2.oldTime()[celli] = savedValuesOld[cell0];
-            }
-        }
-
-        forAll(fineMesh.boundary(), patchi)
-        {
-            if (isA<processorPolyPatch>(fineMesh.boundaryMesh()[patchi]))
-            {
-                field2.boundaryFieldRef()[patchi].initEvaluate(Pstream::commsTypes::nonBlocking);
-                field2.oldTime().boundaryFieldRef()[patchi].initEvaluate(Pstream::commsTypes::nonBlocking);
-            }
-            else
-            {
-                forAll(field2.boundaryFieldRef()[patchi], facei)
-                {
-                    field2.boundaryFieldRef()[patchi][facei] = field1.boundaryField()[patchi][facei];
-                    field2.oldTime().boundaryFieldRef()[patchi][facei] = field1.oldTime().boundaryField()[patchi][facei];
-                }
-            }
-        }
-
+        mapCoarseToFine(*scalarFields_[fieldsi].first(), *scalarFields_[fieldsi].second());
     }
-
+    forAll(vectorFields_, fieldsi)
+    {
+        mapCoarseToFine(*vectorFields_[fieldsi].first(), *vectorFields_[fieldsi].second());
+    }
 }
 
 // ************************************************************************* //
