920d785f28e146935a77b2a95c6a33c38ae894c7
Pierre Horgue <phorgue@gmail.com>
2025-02-28 16:11:55 +0100

    TUT: update plotComparison.py

--- tutorials/scripts/plotComparison.py
+++ tutorials/scripts/plotComparison.py
@@ -8,14 +8,15 @@ Created on Tue Nov  5 14:53:43 2024
 
 # Imports
 from __future__ import with_statement
-import os, subprocess, sys, glob, time, shutil, csv
+import glob, shutil, csv
+from os import path, makedirs
 import numpy as np
 import pylab as plt
 plt.rc('text', usetex=False)
 import re
 
 # Import test case definitions
-from tutorialsList import tutorials as testGroups
+from tutorialsList import tutorials
 
 # Logger class to handle logging
 class Logger:
@@ -25,12 +26,9 @@ class Logger:
 
     def log(self, message):
         """Write message to the log file and print it."""
+        print(message)
         with open(self.logfile, 'a') as f:
             f.write(message + "\n")
-
-# Initialize logger
-logfile = os.path.join(os.getcwd(), 'log.plot')
-logger = Logger(logfile)
 
 # Function to determine the PMF version
 def get_pmf(currentDirectory):
@@ -45,7 +43,7 @@ def get_pmf(currentDirectory):
         else:
             res.append(dir)
     
-    pmfVersionDirectory = os.path.join('/', *res, 'solvers')
+    pmfVersionDirectory = path.join('/', *res, 'solvers')
     target_file = 'headerPMF.H'
     pmfVersion = 'current_version'
 
@@ -57,8 +55,8 @@ def get_pmf(currentDirectory):
         return match.group(1) if match else None
 
     # Verify and extract PMF version
-    file_path = os.path.join(pmfVersionDirectory, target_file)
-    if os.path.exists(file_path):
+    file_path = path.join(pmfVersionDirectory, target_file)
+    if path.exists(file_path):
         pmfVersion = extract_pmf_version(file_path)
         return pmfVersion[:(len(pmfVersion)-1)]
     else:
@@ -66,88 +64,9 @@ def get_pmf(currentDirectory):
         
     return pmfVersion
 
-pmfVersion = get_pmf(os.getcwd())
-
-# Create directory for figures and clean if already exists
-figuresDir = os.path.join(os.getcwd(), "validationFigures")
-if os.path.exists(figuresDir):
-    shutil.rmtree(figuresDir)
-os.makedirs(figuresDir)
-
-# Function to process .gplt files and generate plots
-def plot_gplt(root_dir, solver, case):
-    dataDir = os.path.join(root_dir, solver, case)
-
-    # Find .gplt file in directory
-    def find_gplt_file(root_dir):
-        for root, dirs, files in os.walk(root_dir):
-            for file in files:
-                if file.endswith('.gplt'):
-                    label = file.split('.')[0].split('_')[1]
-                    return (os.path.join(root, file), label)
-        return None
-
-    # Read data from .gplt file
-    def read_gplt_data(file_path):
-        data = []
-        with open(file_path, 'r') as file:
-            lines = file.readlines()
-            for line in lines:
-                try:
-                    x, y = map(float, line.split())
-                    data.append((x, y))
-                except ValueError:
-                    continue
-        return data
-    
-    gpltFile = find_gplt_file(dataDir)
-    if gpltFile:
-        dataFile, dataLabel = gpltFile
-    else:
-        return("[ ERROR ] No data file found\n")
-
-    if not dataFile:
-        print("[ ERROR ] No .gplt file found in saveDatas.\n")
-        logger.log("[ ERROR ] No .gplt file found in saveDatas.\n")
-    else:
-        print(f"[ OK ] Data file found: {dataFile}\n")
-        logger.log(f"[ OK ] Data file found: {dataFile}\n")
-    
-        data = read_gplt_data(dataFile)
-        x = np.array([point[0] for point in data])
-        y = np.array([point[1] for point in data])
-        
-        return(x,y)
-        
-        plt.plot(x/max(x), y/max(abs(y)), label=r"${}$ (pmfv{})".format(dataLabel, pmfVersion))
-        
-        plt.xlabel("Dimensionless height")
-        plt.ylabel(r"Dimensionless ${}$".format(dataLabel))
-        
-        plt.legend()
-        if len(case.split('/')) > 1:
-            strCase = '_'.join(case.split('/')).rstrip('_')
-            plt.savefig(f"{solver}_{strCase}.pdf")
-        else:
-            plt.savefig(f"{solver}_{case}_{dataLabel}.pdf")
-        
-        plt.clf()
-
-    for dir in os.listdir(os.getcwd()):
-        if ".pdf" in dir:
-            shutil.move(os.path.join(os.getcwd(), dir), os.path.join(figuresDir, dir))
 
 # Function to process .csv files and generate plots
-def plot_csv(root_dir, solver, case):
-    dataDir = os.path.join(root_dir, solver, case)
-
-    # Find CSV file in directory
-    def find_csv_file(root_dir):
-        for root, dirs, files in os.walk(root_dir):
-            for file in files:
-                if file.endswith('.csv'):
-                    return os.path.join(root, file)
-        return None
+def extract_XY(main_dir, config):
 
     # Read data from CSV file
     def read_csv_data(file_path):
@@ -166,126 +85,110 @@ def plot_csv(root_dir, solver, case):
 
         return np.array(data_x), np.array(data_y).T, dataLabels
 
-    csvFile = find_csv_file(dataDir)
-    if csvFile:
-        print(f"[ OK ] Data file found: {csvFile}\n")
-        logger.log(f"[ OK ] Data file found: {csvFile}\n")
 
-        x, y_columns, labels = read_csv_data(csvFile)
-        
+    file_path = path.join(main_dir, case, config["result"])
+    resultPresent = path.isfile(file_path)
+
+    if resultPresent:
+        logger.log(f"[ OK ] Data file found: {file_path}\n")
+
+        if config["type"] == "sample":
+            # sample CSV file, get all columns + labels
+            x, y_columns, labels = read_csv_data(file_path)
+        else:
+            labels = [config["label"]]
+            data = np.genfromtxt(file_path)
+            if config["type"] == "steady":
+                x = None
+                y_columns = data[1:]
+            else:
+                x = data[:, 0]
+                y_columns = [data[:, col]]
         
     else:
-        logger.log("[ ERROR ] No .csv file found in saveDatas.\n")
-        print("[ ERROR ] No .csv file found in saveDatas.\n")
+        logger.log("[ ERROR ] File not found: {file_path}\n")
         
     return (x, y_columns, labels)
 
-# Function to process .xy files and generate plots
-def plot_xy(root_dir, solver, case):
-    dataDir = os.path.join(root_dir, solver, case)
-
-    # Find .xy file in directory
-    def find_xy_file(root_dir):
-        for root, dirs, files in os.walk(root_dir):
-            for file in files:
-                if file.endswith('.xy'):
-                    label = file.split('.')[0].split('_')[1]
-                    return (os.path.join(root, file), label)
-        return None
-
-    # Read data from .xy file
-    def read_xy_data(file_path):
-        data = []
-        with open(file_path, 'r') as file:
-            lines = file.readlines()
-            for line in lines:
-                try:
-                    x, y = map(float, line.split())
-                    data.append((x, y))
-                except ValueError:
-                    continue
-        return data
-    
-    xyFile = find_xy_file(dataDir)
-    if xyFile:
-        dataFile, dataLabel = xyFile
-    else:
-        return("[ ERROR ] No data file found\n")
-
-    if not dataFile:
-        print("[ ERROR ] No .gplt file found in saveDatas.\n")
-        logger.log("[ ERROR ] No .gplt file found in saveDatas.\n")
-    else:
-        print(f"[ OK ] Data file found: {dataFile}\n")
-        logger.log(f"[ OK ] Data file found: {dataFile}\n")
-    
-        data = read_xy_data(dataFile)
-        x = np.array([point[0] for point in data])
-        y = np.array([point[1] for point in data])
-        
-    return (x,y)
 
 # RUN routine to process each test group and case
 if __name__ == '__main__':
+
+    # Construct paths
+    script_dir = path.dirname(path.abspath(__file__))
+    tutorials_dir = path.dirname(script_dir)
+    
+    # Initialize logger
+    logfile = path.join(tutorials_dir, 'log.plotComparison')
+    logger = Logger(logfile)
+    
     logger.log("========================================================")
     logger.log("                    PROCESSING DATA                     ")
     logger.log("========================================================\n")
 
-    print("========================================================")
-    print("                    PROCESSING DATA                     ")
-    print("========================================================\n")
-    
-    pmfRefVersion = ''
-    
-    for dir in os.listdir(os.getcwd()):
-        if dir.startswith("references"):
-            pmfRefVersion = dir.split('_')[1]
+    # Create directory for figures and clean if already exists
+    figures_dir = path.join(tutorials_dir, "validationFigures")
 
-    for group in testGroups["tutorials"]:
-        solver = group["solver"]
-        k = 0
+    if path.exists(figures_dir):
+        shutil.rmtree(figures_dir)
+    makedirs(figures_dir)
+    
+    for cfg in tutorials:
+        solver = cfg["solver"]
         
-        print(f"CHECKING FOR {solver}\n")
         logger.log(f"CHECKING FOR {solver}\n")
         
-        for case_info in group["cases"]:
-            case = case_info["case"]
-            dataType = group["dataTypes"][k]
+        for id_case in range(len(cfg["cases"])):
+            case = cfg["cases"][id_case]["case"]
+
+            if not cfg["cases"][id_case]["result"]:
+                logger.log("[ SKIP ] No validation file\n")
+                continue
+
+            if "col" in cfg["cases"][id_case]:
+                col = cfg["cases"][id_case]["col"]
+            else:
+                col=1
             
-            print(f"CASE : {case}\n")
             logger.log(f"CASE : {case}\n")
             
             # Call respective plot function based on data type    
-            if dataType['dataType'] == '.csv':
-                try:
-                    x, y_columns, labels = plot_csv(os.path.join(os.getcwd(), 'currentResults'), solver, case)
-                    xRef, y_columnsRef, labelsRef = plot_csv(os.path.join(os.getcwd(), 'referenceResults'), solver, case)
-                    
+            try:
+                x, y_columns, labels = extract_XY(path.join(tutorials_dir,solver+"-tutorials"),
+                                                  cfg["cases"][id_case])
+                xRef, y_columnsRef, labelsRef = extract_XY(path.join(tutorials_dir,"referenceResults",solver),
+                                                           cfg["cases"][id_case])
+
+                if cfg["cases"][id_case]["type"] == "steady":
+                    fig, ax = plt.subplots(figsize=(6,6), dpi=300)
+                    plt.plot(y_columnsRef, y_columns, 'kx')
+                    plt.plot([y_columnsRef[0],y_columnsRef[-1]],
+                             [y_columnsRef[0],y_columnsRef[-1]], 'b-')
+                    plt.xlabel("Reference result")
+                    plt.ylabel("Current result")
+                else:
                     i = 0
+
                     for idx, y in enumerate(y_columns):
-                        plt.plot(xRef / max(xRef), y / max(abs(y_columnsRef[i])), label="reference results", ls='-')
-                        plt.plot(x / max(x), y / max(abs(y)), label="current results", ls='--')
-                        plt.xlabel("Dimensionless height")
+                        fig, ax = plt.subplots(figsize=(6,6), dpi=300)
+                        x_max = max(xRef)
+                        y_max = max(abs(y_columnsRef[i]))
+                        plt.plot(xRef/x_max, y_columnsRef[i]/y_max, label="reference results", ls='-')
+                        plt.plot(x/x_max, y/y_max, label="current results", ls='--')
+                        plt.xlabel("Dimensionless (height or time)")
                         plt.ylabel(r"Dimensionless {}".format(labels[idx]))
                         plt.legend()
-            
+                        plt.title(solver+" - "+case)
                         filename = f"{solver}_{'_'.join(case.split('/'))}_{labels[idx]}.pdf" if len(case.split('/')) > 1 else f"{solver}_{case}_{labels[idx]}.pdf"
-                        plt.savefig(filename)
-                        plt.clf()
+                        plt.savefig(path.join(figures_dir,filename))
+                        plt.close()
                         
                         i += 1
-            
-                    for pdf_file in glob.glob("*.pdf"):
-                        shutil.move(pdf_file, os.path.join(figuresDir, pdf_file))
-                except Exception as e:
-                    print(f"[ ERROR ] Encountered error : {e} ")
-                    logger.log(f"[ ERROR ] Encountered error : {e} ")
-            k += 1
+
+            except Exception as e:
+                print(f"[ ERROR ] Encountered error : {e} ")
+                logger.log(f"[ ERROR ] Encountered error : {e} ")
 
     logger.log("========================================================")
     logger.log("                        FINISHED                        ")
     logger.log("========================================================\n")
-
-    print("========================================================")
-    print("                        FINISHED                        ")
-    print("========================================================\n")
--- tutorials/scripts/tutorialsList.py
+++ tutorials/scripts/tutorialsList.py
@@ -2,122 +2,100 @@
 
 import json
 
-tutorials = {
-    "tutorials": [
-        {
-            "solver": "anisoImpesFoam",
-            "cases": [
-                { "case": "injectionAniso_case1" },
-                { "case": "injectionAniso_case2" }
-            ],
-            "dataTypes": [
-                { "dataType": ".csv" },
-                { "dataType": ".csv" },
-            ]
-        },
-        {
-            "solver": "impesFoam",
-            "cases": [
-                { "case": "Buckley-Leverett/BrooksAndCorey" },
-                { "case": "Buckley-Leverett/VanGenuchten" },
-                { "case": "capillarityValidation/BrooksAndCorey" },
-                { "case": "capillarityValidation/VanGenuchten" },
-                { "case": "injectionExtraction/injection" },
-                { "case": "injectionExtraction/extraction" }
-            ],
-            "dataTypes": [
-                { "dataType": ".csv" },
-                { "dataType": ".csv" },
-                { "dataType": ".csv" },
-                { "dataType": ".csv" },
-                { "dataType": ".csv" },
-                { "dataType": ".csv" },
-            ]
-        },
-        {
-            "solver": "darcyFoam",
-            "cases": [
-                { "case": "SPE10" }
-            ],
-            "dataTypes": [
-                { "dataType": ".csv" }
-            ]
-        },
-        {
-            "solver": "groundwater2DFoam",
-            "cases": [
-                { "case": "steadyFlow" },
-                { "case": "transientFlow" }
-            ],
-            "dataTypes": [
-                { "dataType": "potential" },
-                { "dataType": "potential" }
-            ]
-        },
-        {
-            "solver": "groundwaterFoam",
-            "cases": [
-                { "case": "1Dinfiltration" },
-                { "case": "1Dinfiltration_dualPorosity" },
-                { "case": "steadyFlow" },
-                { "case": "transientFlow" }
-            ],
-            "dataTypes": [
-                { "dataType": ".csv" },
-                { "dataType": ".csv" },
-                { "dataType": ".csv" },
-                { "dataType": ".csv" }
-            ]
-        },
-        {
-            "solver": "groundwaterTransport2DFoam",
-            "cases": [
-                { "case": "coupled" }
-            ],
-            "dataTypes": [
-                { "dataType": "C" },
-                { "dataType": "potential" }
-            ]
-        },
-        {
-            "solver": "groundwaterTransportFoam",
-            "cases": [
-                { "case": "1Dinfiltration_dualPorosity" },
-                { "case": "coupled" }
-            ],
-            "dataTypes": [
-                { "dataType": ".csv" },
-                { "dataType": ".csv" }
-            ]
-        },
-        {
-            "solver": "porousScalarTransport2DFoam",
-            "cases": [
-                { "case": "transport" }
-            ],
-            "dataTypes": [
-                { "dataType": "C" },
-            ]
-        },
-        {
-            "solver": "porousScalarTransportFoam",
-            "cases": [
-                { "case": "1DeventFlux_Euler" },
-                { "case": "1DeventFlux_backward" },
-                { "case": "1DeventFlux_CrankNicolson" },
-                { "case": "1DeventFlux_multispecies" },
-                { "case": "transport" }
-            ],
-            "dataTypes": [
-                { "dataType": ".csv" },
-                { "dataType": ".csv" },
-                { "dataType": ".csv" },
-                { "dataType": ".csv" },
-                { "dataType": ".csv" }
-            ]
-        }
-    ]
-}
+tutorials = [
+    {
+        "solver": "anisoImpesFoam",
+        "cases": [
+            { "case": "injectionAniso_case1", "type": "sample",
+              "result": "postProcessing/sampleDict/2500/acrossFlow_Sb.csv" },
+            { "case": "injectionAniso_case2", "type": "sample",
+              "result": "postProcessing/sampleDict/2500/acrossFlow_Sb.csv" }
+        ]
+    },
+    {
+        "solver": "impesFoam",
+        "cases": [
+            { "case": "Buckley-Leverett/BrooksAndCorey", "type": "sample",
+              "result": "postProcessing/sampleDict/20000/acrossFlow_Sb.csv" },
+            { "case": "Buckley-Leverett/VanGenuchten", "type": "sample",
+              "result": "postProcessing/sampleDict/30000/acrossFlow_Sb.csv" },
+            { "case": "capillarityValidation/BrooksAndCorey", "type": "sample",
+              "result": "postProcessing/sampleDict/1000/acrossFlow_Sb.csv" },
+            { "case": "capillarityValidation/VanGenuchten", "type": "sample",
+              "result": "postProcessing/sampleDict/1000/acrossFlow_Sb.csv" },
+            { "case": "injectionExtraction/injection", "type": "sample",
+              "result": "postProcessing/sampleDict/25000/acrossFlow_Sb.csv" },
+            { "case": "injectionExtraction/extraction", "type": "sample", 
+              "result": "postProcessing/sampleDict/25000/acrossFlow_Sb.csv" }
+        ]
+    },
+    {
+        "solver": "darcyFoam",
+        "cases": [
+            { "case": "SPE10",  "type": None, "result": None }
+        ]
+    },
+    {
+        "solver": "groundwater2DFoam",
+        "cases": [
+            { "case": "steadyFlow", "type": "steady", "label": "potential",
+              "result": "potential_probes.dat"},
+            { "case": "transientFlow", "type": "probe", "label": "potential",
+              "result": "postProcessing/probes/0/potential", "col": 5}
+        ]
+    },
+    {
+        "solver": "groundwaterFoam",
+        "cases": [
+            { "case": "1Dinfiltration", "type": "sample",
+              "result": "postProcessing/sampleDict/50000/acrossFlow_h.csv"},
+            { "case": "1Dinfiltration_dualPorosity", "type": "sample",
+              "result": "postProcessing/sampleDict/3456/acrossFlow_theta_thetaMatrix.csv"},
+            { "case": "steadyFlow", "type": "sample",
+              "result": "postProcessing/sampleDict/159/acrossFlow_theta.csv"},
+            { "case": "transientFlow", "type": "transient", "label": "outflow",
+              "result": "waterMassBalance.csv", "col": 2}
+        ]
+    },
+    {
+        "solver": "groundwaterTransport2DFoam",
+        "cases": [
+            { "case": "coupled", "type": "probe", "label": "concentration",
+              "result": "postProcessing/probes/0/C", "col": 5}
+        ]
+    },
+    {
+        "solver": "groundwaterTransportFoam",
+        "cases": [
+            { "case": "1Dinfiltration_dualPorosity", "col": 5, "type": "sample",
+              "result": "postProcessing/sampleDict/3456/acrossFlow_C_CMatrix_theta_thetaMatrix.csv"},
+            { "case": "coupled", "type": "transient", "label": "Coutflow",
+              "result": "CmassBalance.csv", "col": 3}
+        ]
+    },
+    {
+        "solver": "porousScalarTransport2DFoam",
+        "cases": [
+            { "case": "transport", "type": "probe", "label": "C_outflow",
+             "result": "postProcessing/probes/0/C", "col": 5 },
+        ]
+    },
+    {
+        "solver": "porousScalarTransportFoam",
+        "cases": [
+            { "case": "1DeventFlux_Euler", "type": "transient", "label": "Coutflow",
+              "result": "CmassBalance.csv", "col": 3},
+            { "case": "1DeventFlux_backward", "type": "transient", "label": "Coutflow",
+              "result": "CmassBalance.csv", "col": 3},
+            { "case": "1DeventFlux_CrankNicolson", "type": "transient", "label": "Coutflow",
+              "result": "CmassBalance.csv", "col": 3},
+            { "case": "1DeventFlux_multispecies", "type": "transient", "label": "Coutflow",
+              "result": "C1massBalance.csv", "col": 3},
+            { "case": "transport", "type": "transient", "label": "Coutflow",
+              "result": "CmassBalance.csv", "col": 3}
+        ]
+    }
+]
 
 # Convertit la structure en une chaîne JSON formatée
 json_output = json.dumps(tutorials, indent=4)
